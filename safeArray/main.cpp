7b1a1433256380990d857a89bd1374597e0becb1
Maxwell <maxperryg@gmail.com>
2018-10-29 14:36:01 -0400

    Initial Commit

---
+++ safeArray/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  safeArray
+//
+//  Created by Maxwell Perry Grossman on 10/29/18.
+//  Copyright © 2018 Maxwell Perry Grossman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
