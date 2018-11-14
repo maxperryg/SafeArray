ffcecd7bd08da9e661c8a1e5fc76a498e4b22dc4
Maxwell <maxperryg@gmail.com>
2018-11-14 14:15:31 -0500

    overloaded * operator fully works and changes parameters for proxy constructor to take horizontal bounds for error checking on [][]

--- safeArray/main.cpp
+++ safeArray/main.cpp
@@ -137,11 +137,12 @@ public:
     class Proxy {
         private:
             T* array;
-        int lowI;
+            int lowI, highI;
         public:
-            Proxy(T* pass, int low){
+            Proxy(T* pass, int low, int high){
                 array = pass;
                 lowI = low;
+                highI= high;
             }
         
             ~Proxy(){
@@ -149,6 +150,10 @@ public:
             }
         
             T& operator[](int index) {
+                if(index<lowI || index>highI){
+                    cout<<"index "<<index<<" out of range"<<endl;
+                    exit(1);
+                }
                 return array[index-lowI];
             }
     };
@@ -159,19 +164,26 @@ public:
             exit(1);
         }
         row = r-verticalLow;
-        Proxy proxy(p[row],horizontalLow);
+        Proxy proxy(p[row],horizontalLow, horizontalHigh);
         return proxy;
     }
     
-//    SafeArray<T> operator*(const SafeArray &s){
-//        if(width!= s.height){
-//            cout<<"The width of the first and the height of the second need to be the same"<<endl;
-//            exit(1);
-//        }
-//        SafeArray<T> answer(height,s.width);
-//        
-//        
-//    }
+    SafeArray<T> operator*(const SafeArray &s){
+        if(width!= s.height){
+            cout<<"The width of the first and the height of the second need to be the same"<<endl;
+            exit(1);
+        }
+        SafeArray<T> answer(height,s.width);
+        for (int r = 0; r < answer.height; r++) {
+            for (int c = 0; c < answer.width; c++) {
+                answer[r][c]=0;
+                for (int in = 0; in < answer.height; in++) {
+                    answer[r][c] += p[r][in]*s.p[in][c];
+                }
+            }
+        }
+        return answer;
+    }
     
     template <class U>
     friend ostream& operator<<(ostream& os, SafeArray<U> s);
@@ -190,13 +202,24 @@ ostream& operator<<(ostream& os, SafeArray<T> s){
 
 
 int main(){
-    SafeArray<int> intArray(5,7, 5, 7);
+    SafeArray<int> intArrayA(5,7, 5, 7);
     for(int i=5;i<=7;i++){
         for(int j=5;j<=7;j++){
-            intArray[i][j]=j;
+            intArrayA[i][j]=j;
         }
     }
-    cout<<intArray;
+    cout<<intArrayA<<endl;
+    
+    SafeArray<int> intArrayB(5,7, 5, 7);
+    for(int i=5;i<=7;i++){
+        for(int j=5;j<=7;j++){
+            intArrayB[i][j]=j;
+        }
+    }
+    cout<<intArrayB<<endl;
+    
+    cout<<intArrayA*intArrayB<<endl;
+    
 //    SafeArray<string> stringArray(10);
 //    for(int i=0;i<10;i++){
 //        stringArray[i]="string";
